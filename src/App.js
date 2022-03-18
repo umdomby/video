500ce6c48d9944a2ec11ca2ad7690065bbeb7e64
navatar <umdomby@yandex.ru>
2022-03-18 18:34:12 +0300

    start servicerobot.pro

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const App = () => {
   }
 
   useEffect(() => {
-    signal = new IonSFUJSONRPCSignal("wss://servicerobot.pro:4434/ws");
+    signal = new IonSFUJSONRPCSignal("wss://servicerobot.pro:4435/ws");
     client = new Client(signal, config);
     signal.onopen = () => client.join("test room");
 
